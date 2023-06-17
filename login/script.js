05c580bd98fa65bb8c4029a920e74b97586772dc
AEntropia <127155096+AEntropia@users.noreply.github.com>
2023-06-17 15:27:16 -0300

    v1

---
+++ login/script.js
@@ -0,0 +1,12 @@
+function Isadmin(){
+    email = document.getElementById("form2Example1").value
+    ad = email.split("@")
+    const a = ad[1].substring(0, 5)
+    console.log(a)
+    if (a=="admin"){
+        window.open('/administrar/index.html', '_self')
+    }
+    else{
+        window.open('/extrato/index.html', '_self')
+    }
+}
